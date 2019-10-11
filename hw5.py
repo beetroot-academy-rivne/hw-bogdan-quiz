629c3afc805ad6f5e51eec73621fc46584ebfc5f
Kolosovsky <aspirius193@gmail.com>
2019-10-11 11:51:15 +0300

    commit

--- hw5.py
+++ hw5.py
@@ -1,1 +1,16 @@
-1
+from random import randint
+
+l1 = list()
+
+
+for i in range(0,10):
+	l1.append(randint(1,100))	
+
+print(l1)
+
+l2 = list()
+
+for i in range(0,10):
+	if not l1[i] % 7 and l1[i] % 5:
+		l2.append(l1[i])
+print(l2)
