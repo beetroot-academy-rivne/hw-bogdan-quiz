514abab21bf6d72723f35f92eaa1ae1bf7d70a74
Kolosovsky <aspirius193@gmail.com>
2019-10-20 21:49:30 +0300

    Functions hw on saturday

---
+++ hw9.py
@@ -0,0 +1,29 @@
+# 1. A simple function
+# def favorite_movie(movie_name):
+# 	print('My favourite movie is', movie_name)
+# favorite_movie('Rembo')
+
+# 2. Creating a dictionary.
+# def make_country(name, capital):
+# 	country = dict()
+# 	country['name'] = name
+# 	country['capital'] = capital
+# 	print(country)
+# make_country('Ukraine', 'Kyiv')
+
+# 3. A simple calculator
+# def make_operation(operation, *nums):
+# 	result = 0
+# 	if operation == '+':
+# 		for num in nums:
+# 			result += num
+# 	elif operation == '-':
+# 		for num in nums:
+# 			result -= num
+# 	elif operation == '*':
+# 		result = 1
+# 		for num in nums:
+# 			result *= num
+# 	return result
+# print(make_operation('+', 1 ,2, 3))
+
